41de1a96b51f7968c218c5b24f10fc04ffb303dd
faisal <faisalparveez@gmail.com>
2015-02-15 10:43:27 -0600

    replaced more recommended use samples

--- routes.py
+++ routes.py
@@ -1,22 +1,20 @@
 from webapp2_extras.routes import RedirectRoute, PathPrefixRoute, DomainRoute
+from services import sample_jsonrpc
 from web import admin, handlers
 
 _routes = [
-    # Sample for administering
     PathPrefixRoute('/admin',[
         RedirectRoute('/', admin.HomeHandler, name='admin-home', strict_slash=True),
     ]),
 
     PathPrefixRoute('/user',[
         RedirectRoute('/login', handlers.LoginHandler, name='user-login', strict_slash=True),
-        # Lazy loading sample
-        RedirectRoute('/logout', 'web.handlers.LogoutHandler', name='user-logout', strict_slash=True)
+        RedirectRoute('/logout', handlers.LogoutHandler, name='user-logout', strict_slash=True)
     ]),
 
     # Main Routes
     RedirectRoute('/', handlers.HomeHandler, name='home', strict_slash=True),
-    # Another Lazy loading sample, will only be loaded once its routed
-    RedirectRoute('/rpc', 'services.sample_jsonrpc.ApiHandler', name='rpc', strict_slash=True)
+    RedirectRoute('/rpc', sample_jsonrpc.ApiHandler, name='rpc', strict_slash=True)
 ]
 
 
